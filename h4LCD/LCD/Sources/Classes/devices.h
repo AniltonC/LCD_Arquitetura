84a88d8a5365b2426d297fc868f3b75fd04b9566
Anilton HomePC <aclr@icomp.ufam.edu.br>
2019-12-01 18:57:29 -0400

    Classe Visor Finalizada

--- h4LCD/LCD/Sources/Classes/devices.h
+++ h4LCD/LCD/Sources/Classes/devices.h
@@ -39,35 +39,95 @@ public:
 class Visor
 {
 private:
-    /*uint8_t adress;
-    functionSetOption linhas, colunas, pixels;
-    i2c_Pin_t SCL_pin, SDA_pin;
-    mkl_LCD lcd(linhas, colunas, pixels, SCL_pin, SDA_pin, adress);*/
     mkl_LCD lcd;
+    void init_printTime()
+    {
+        uint8_t minuto[4] = {32, 32, 32, 32};
+        lcd.home();
+        for (uint8_t i = 0; i < 4; i++)
+        {
+            lcd.putChar(minuto[i]);
+            if (i == 1)
+                lcd.putChar(':');
+            lcd.delay(50);
+        }
+    }
+
+    void init_printCook()
+    {
+        string cook = "          ";
+        lcd.setCursor(1, 6);
+        lcd.putChar(32);
+        lcd.delay(50);
+        for (uint8_t i = 0; i < cook.length(); i++)
+        {
+            lcd.putChar(cook[i]);
+            lcd.delay(50);
+        }
+    }
+
+    void init_printOper()
+    {
+        string porta = "P-    ";
+        string valv = "V-    ";
+        string giro = "G-   ";
+        for (uint8_t i = 0; i < porta.length(); i++)
+        {
+            lcd.setCursor(2, 1 + i);
+            lcd.putChar(porta[i]);
+            lcd.setCursor(2, 7 + i);
+            lcd.putChar(valv[i]);
+            if (13 + i < 32)
+            {
+                lcd.setCursor(2, 13 + i);
+                lcd.putChar(giro[i]);
+            }
+            lcd.delay(50);
+        }
+    }
 
 public:
-    /*Visor(functionSetOption numLines, functionSetOption numCols,
-          functionSetOption numPixels, i2c_Pin_t SCLPin, i2c_Pin_t SDAPin,
-          uint8_t I2CAddress)
-    {
-        linhas = numLines;
-        colunas = numCols;
-        pixels = numPixels;
-        SCL_pin = SCLPin;
-        SDA_pin = SDAPin;
-        adress = I2CAddress;
-    }*/
     Visor(functionSetOption numLines, functionSetOption numCols,
           functionSetOption numPixels, i2c_Pin_t SCLPin, i2c_Pin_t SDAPin,
           uint8_t I2CAddress) : lcd(numLines, numCols, numPixels, SCLPin, SDAPin, I2CAddress)
     {
-        lcd.setCursor(1, 7);
-        lcd.putString("LCD");
-        lcd.setCursor(2, 3);
-        lcd.putString("INICIALIZADO");
-        lcd.delay(5000);
-        lcd.clear();
         lcd.home();
+        for (uint8_t i = 0; i < 32; i++)
+        {
+            lcd.putChar('.');
+            if (i == 15)
+                lcd.setCursor(2, 1);
+            lcd.delay(50);
+        }
+        init_printTime();
+        init_printCook();
+        init_printOper();
+    }
+    void printTime(uint8_t minuto[4])
+    {
+        lcd.home();
+        for (uint8_t i = 0; i < 4; i++)
+        {
+            lcd.putChar(minuto[i]);
+            if (i == 1)
+                lcd.putChar(':');
+        }
+    }
+
+    void printCook(string cook)
+    {
+        lcd.setCursor(1, 9);
+        lcd.putString(cook);
+    }
+
+    void printOper(string porta, string valv, string giro)
+    {
+        lcd.setCursor(2, 1);
+        lcd.putString("P-" + porta);
+        lcd.setCursor(2, 7);
+        lcd.putString("V-" + valv);
+        lcd.setCursor(2, 13);
+        lcd.putString("G-" + giro);
     }
 };
 
